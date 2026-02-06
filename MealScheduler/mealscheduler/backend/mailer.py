fb7a5e66c2ad7dfd28941d84930c07e692cf2d53
k.seitz-admin <kevinseitz2005@gmail.com>
2026-02-06 13:20:00 +0100

    Refactor mailer.py to improve readability and ensure RECIPIENT is sourced from .env

--- MealScheduler/mealscheduler/backend/mailer.py
+++ MealScheduler/mealscheduler/backend/mailer.py
@@ -3,23 +3,26 @@ import smtplib
 from email.message import EmailMessage
 from dotenv import load_dotenv
 
-# .env laden
+# load .env 
 load_dotenv()
 
+# call Data from .env
 GMAIL_USER = os.getenv("GMAIL_USER")
 GMAIL_APP_PASSWORD = os.getenv("GMAIL_APP_PASSWORD")
+RECIPIENT = os.getenv("RECIPIENT")
 
+# Check if credentials are available
 if not GMAIL_USER or not GMAIL_APP_PASSWORD:
     raise RuntimeError("GMAIL Zugangsdaten fehlen (.env prüfen)")
 
-# E-Mail erstellen
+# create E-Mail 
 msg = EmailMessage()
 msg["From"] = GMAIL_USER
-msg["To"] = ""
+msg["To"] = RECIPIENT
 msg["Subject"] = "Testmail aus Python"
 msg.set_content("Hallo!\n\nDiese Mail wurde mit Python gesendet.")
 
-# SMTP-Verbindung (Gmail)
+# SMTP-Connection and send mail
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
     server.login(GMAIL_USER, GMAIL_APP_PASSWORD)
     server.send_message(msg)

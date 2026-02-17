35293fae658d08c0e3cfe997a177557fbf21e3c3
ThatsMe <kevinseitz2005@gmail.com>
2026-02-17 14:42:12 +0000

    db_dml.py: Entered SQL Statements to create, delete and edit

--- backend/db_dml.py
+++ backend/db_dml.py
@@ -20,82 +20,104 @@ class Repository(ABC):
 class RecipesRepository(Repository):
 
     def add(self, name, ingredients, steps, tags=None):
+        sql = "INSERT INTO recipes (name) VALUES (?)"
         print("adding recipe")
+        
 
     def delete(self, recipe_id):
+        sql = "DELETE FROM recipes WHERE id = ?"
         print("deleting recipe")
 
     def edit(self, recipe_id, **changes):
+        sql = "UPDATE recipes SET "
         print("editing recipe")
 
 
 class IngredientsRepository(Repository):
     
     def add(self):
+        sql = "INSERT INTO ingredients (name) VALUES (?)"
         print("adding Ingredient")
         
     def delete(self):
+        sql = "DELETE FROM ingredients WHERE id = ?"
         print("deleting ingredient")
         
     def edit(self):
+        sql = "UPDATE ingredients SET name = ? WHERE id = ?"
         print("editing ingredient")
     
     
 class StepsRepository(Repository):
     
     def add(self):
+        sql = "INSERT INTO steps (recipe_id, step_number, description) VALUES (?, ?, ?)"
         print("adding Step")
         
     def delete(self):
+        sql = "DELETE FROM steps WHERE id = ?"
         print("deleting Step")
         
     def edit(self):
+        sql = "UPDATE steps SET step_number = ?, description = ? WHERE id = ?"
         print("editing Step")
     
     
 class TagsRepository(Repository):
     
     def add(self):
+        sql = "INSERT INTO tags (name) VALUES (?)"
         print("adding Tag")
         
     def delete(self):
+        sql = "DELETE FROM tags WHERE id = ?"
         print("deleting Tag")
         
     def edit(self):
+        sql = "UPDATE tags SET name = ? WHERE id = ?"
         print("editing Tag")
     
     
 class UnitsRepository(Repository):
     
     def add(self):
+        sql = "INSERT INTO units (name, abbr) VALUES (?, ?)"
         print("adding Unit")
         
     def delete(self):
+        sql = "DELETE FROM units WHERE id = ?"
         print("deleting Unit")
         
     def edit(self):
+        sql = "UPDATE units SET name = ?, abbr = ? WHERE id = ?"
         print("editing Unit")
     
     
 class RecipeIngredientsRepository(Repository):
     
     def add(self):
+        sql = "INSERT INTO recipe_ingredients (recipe_id, ingredient_id, quantity, unit_id, note, position) VALUES (?, ?, ?, ?, ?, ?)"
         print("adding Recipe/Ingredient")
         
     def delete(self):
+        sql = "DELETE FROM recipe_ingredients WHERE id = ?"
         print("deleting Recipe/Ingredient")
         
     def edit(self):
+        sql = "UPDATE recipe_ingredients SET recipe_id = ?, ingredient_id = ?, quantity = ?, unit_id = ?, note = ?, position = ? WHERE id = ?"
         print("editing Recipe/Ingredient")
     
     
 class RecipeTagsRepository(Repository):
     
     def add(self):
+        sql = "INSERT INTO recipe_tags (recipe_id, tag_id) VALUES (?, ?)"
         print("adding Recipe/Tag")
         
     def delete(self):
+        sql = "DELETE FROM recipe_tags WHERE recipe_id = ? AND tag_id = ?"
         print("deleting Recipe/Tag")
         
     def edit(self):
+        sql = "UPDATE recipe_tags SET tag_id = ? WHERE recipe_id = ? AND tag_id = ?"
         print("editing Recipe/Tag")
